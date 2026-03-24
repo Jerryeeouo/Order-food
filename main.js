427b0c9b20fef20da7b76c93d16e8ee06b0221da
Jerryeeouo <jerryyanghi0209@gmail.com>
2026-03-24 22:23:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function getnum(foodList,num,havedraw = false) {
     found = found.filter(num => !blacklist.includes(num))
     
     found.forEach(seat => {
-      if (!alreadyPick.includes(seat) && !results.includes(seat)) {
+      if (!alreadyPick.includes(seat) && !results.map(data => data.id).includes(seat)) {
         Numbers.push(seat);
       }
     });
